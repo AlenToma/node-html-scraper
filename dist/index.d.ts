364fe74518edea31bdee767a8ce4404b8d49eee5
Alen Toma <alen.toma@gmail.com>
2022-02-02 10:59:17 +0100

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ import { ISelector, IObjectContructor } from '../src/types'
 
 
 declare namespace Parser {
-    const parseHtml: (html: string, baseUrl: string) => ISelector;
+    const parseHtml: (html: string, baseUrl?: string) => ISelector;
     const toObject: <T>(jq: ISelector) => IObjectContructor<T>;
 }
 
